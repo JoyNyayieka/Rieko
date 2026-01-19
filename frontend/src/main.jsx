2b3373f4f24fdb1cfe104abffbae3523878271f0
Mitch Nyayieka <joynyayieka@gmail.com>
2026-01-19 22:40:13 +0300

    config: set up Mantine, notifications and routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,32 @@
-import './index.css'
 import App from './App.jsx'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 import { MantineProvider } from '@mantine/core'
+import { Notifications } from '@mantine/notifications'
+
 import '@mantine/core/styles.css'
+import "@mantine/notifications/styles.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MantineProvider>
-      <App />
-    </MantineProvider>    
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <MantineProvider
+        theme={{
+          components: {
+            Notification: {
+              defaultProps: {
+                color: "#3A5B22"
+              },
+            },
+          },
+          fontFamily: "CrimsonPro, sans-serif",
+        }}
+      >
+        <Notifications position='top-right' />
+        <App />
+      </MantineProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
