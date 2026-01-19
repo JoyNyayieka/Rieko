968095f3dc9b672c5d7a266f9440ee4036e6318a
Mitch Nyayieka <joynyayieka@gmail.com>
2026-01-19 22:34:47 +0300

    feat: add routing to App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,5 @@
-import { useState, useEffect } from 'react';
-import { Container, Title, Button, Text } from '@mantine/core';
-import api from './api/axios';
+import AppRoutes from "./routes/AppRoutes";
 
-function App() {
-  const [message, setMessage] = useState('');
-
-  const fetchMessage = async () => {
-    try {
-      const response = await api.get('myapp/hello/');
-      setMessage(response.data.message);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  };
-
-  return (
-    <Container size="sm" mt={40}>
-      <Title order={1}>Axios Functionality</Title>
-      <Button onClick={fetchMessage} mt={20}>
-        Fetch Message from Django
-      </Button>
-      {message && <Text mt={20}>{message}</Text>}
-    </Container>
-  );
+export default function App() {
+  return <AppRoutes />;
 }
-
-export default App;
