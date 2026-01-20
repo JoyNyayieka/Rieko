90fc4e3efd3ed1a1f9e002608d8d7b791c993974
Mitch Nyayieka <joynyayieka@gmail.com>
2026-01-20 21:51:55 +0300

    feat: create landing page in pages folder

---
+++ frontend/src/pages/landing/LandingPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import LandingLayout from '../../layouts/LandingLayout'
+
+export default function LandingPage() {
+  return (
+    <LandingLayout>
+        
+    </LandingLayout>
+  )
+}
