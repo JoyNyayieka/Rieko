d7375cb90a2f161202c883123ba9b83d53ea46a1
Mitch Nyayieka <joynyayieka@gmail.com>
2026-02-05 23:44:15 +0300

    fix: delete overwriding font in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -21,7 +21,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
               },
             },
           },
-          fontFamily: "CrimsonPro, sans-serif",
+          
         }}
       >
         <Notifications position='top-right' />
