d9158df60a6c112eee4a514d87743e13e949828c
Mitch Nyayieka <joynyayieka@gmail.com>
2026-02-23 23:29:31 +0300

    feat: add landing pg components

--- frontend/src/pages/landing/LandingPage.jsx
+++ frontend/src/pages/landing/LandingPage.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import LandingLayout from '../../layouts/LandingLayout'
+import Hero from '../../components/landingpage/Hero/Hero'
+import About from '../../components/landingpage/About'
+import FAQs from '../../components/landingpage/FAQs'
+import ContactUI from '../../components/landingpage/ContactUI/ContactUI'
+import ExpertCTA from '../../components/landingpage/ExpertCTA'
+
 
 export default function LandingPage() {
   return (
     <LandingLayout>
-        
+        <Hero />
+        <About/>
+        <FAQs/>
+        <ExpertCTA/>
+        <ContactUI/>
     </LandingLayout>
   )
 }
